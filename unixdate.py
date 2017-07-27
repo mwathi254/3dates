3650ae3b9ab0a401d4b793bed26f8071032672d1
mwathi254 <30492141+mwathi254@users.noreply.github.com>
2017-07-27 14:37:26 +0300

    Update unixdate.py

--- unixdate.py
+++ unixdate.py
@@ -15,21 +15,14 @@ Your End Date
 import time
 import datetime
 
-
-    
 startTime = datetime.date(input('Enter Year: '),input('Enter Month: '),input('Enter Day: '))
 endTime = datetime.date(input('Enter Year: '),input('Enter Month: '),input('Enter Day: '))
 d = datetime.datetime.now()
 dEdit = d.replace(hour=0,minute=0,second=0,microsecond=0)
-
-
 nowDate = time.mktime(dEdit.timetuple())
 startDate = time.mktime(startTime.timetuple())
-endDate = time.mktime(endTime.timetuple())
-            
-        
-            
-            
+endDate = time.mktime(endTime.timetuple())            
 print "unix of todays date is: ",(nowDate)
 print "unix of first date is: ",(startDate)
 print "unix of last date is: ",(endDate)
+#eNd
